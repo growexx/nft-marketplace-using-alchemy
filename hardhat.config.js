5cb9538d431dc0acb50dd167b7b00d5a8c7383ef
Ravi Shrimali <rshrimali@parkstreet.com>
2023-01-25 17:59:38 +0530

    Commit Code

---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+const { API_URL, PRIVATE_KEY } = process.env;
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
+      //accounts: [process.env.privateKey]
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
+      //accounts: [process.env.privateKey]
+    },
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
---
+++ src/pinata.js
@@ -0,0 +1,95 @@
+//require('dotenv').config();
+//Old One
+//const key = "648299ad0d29fec3b4d4"
+//const secret = "b836a83ace680941c105ef1b52db237d5ce7f1ca9a2ece9f3266a03f8c979ff2";
+//New
+const key = "087ebd482562e6518a6c"
+const secret = "ce46b91e89fab997bb7f1402f83be66e6e279590d0d6c868b3723bc9265ce4c9";
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+            return {
+                success: true,
+                pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+            };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async (file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
